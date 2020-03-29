aacf2b98294a0e654d55e6ac5f85539679c7ed0b
abolbas <test@mail.ru>
2020-03-29 20:01:15 +0300

    xml mapping. Lesson 2

--- src/main/java/by/bolbas/study/util/HibernateUtil.java
+++ src/main/java/by/bolbas/study/util/HibernateUtil.java
@@ -6,10 +6,10 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
-    private static SessionFactory sessionFactory = null;
+    private static SessionFactory sessionFactory;
 
     static {
-        Configuration cfg = new Configuration().configure();
+        Configuration cfg = new Configuration().configure("config/hibernate.cfg.xml");
         StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
                                                         .applySettings(cfg.getProperties());
 
